06b0b0bb8f476ee2d8baf76f497ac340a822ac5b
Evgenii Pasechnickov <evgenii.pase4nickov@gmail.com>
2024-12-16 14:03:26 +0300

    dev: create input

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
-import { MyForm } from './components/form';
+import React, { Component } from "react";
+import { Child } from "./Child";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <MyForm/>
-      </header>
-    </div>
-  );
+
+const ItemList = ({ items }) => (
+  <ul>
+    {items.map((item, index) => (
+      <li key={index}>{item}</li> 
+    ))}
+  </ul>
+);
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      items: ['Item 1', 'Item 2', 'Item 3'], 
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleFocusInput = this.handleFocusInput.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount: Компонент смонтирован');
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate: Компонент был обновлён');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount: Компонент будет размонтирован');
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    console.log('Submitted name:', this.state.name);
+  }
+
+  handleChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleFocusInput() {
+    this.inputRef.focus();
+  }
+
+  render() {
+    const isReactIncluded = this.state.name.toLowerCase().includes('реакт');
+
+    return (
+      <React.Fragment>
+        <h1>Форма ввода имени</h1>
+        <form>
+          <label>
+            Имя:
+            <input
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+              ref={(ref) => (this.inputRef = ref)} 
+            />
+          </label>
+          <button 
+            type="submit" 
+            onClick={this.handleClick} 
+            disabled={isReactIncluded} 
+          >
+            Отправить
+          </button>
+          <button type="button" onClick={this.handleFocusInput}>Фокус на инпут</button>
+        </form>
+        {this.state.name && <p>Вы ввели: {this.state.name}</p>}
+        <Child name={this.state.name} age={21} isActive={true} />
+        
+       
+        <ItemList items={this.state.items} />
+      </React.Fragment>
+    );
+  }
 }
 
-export default App;
+
+
